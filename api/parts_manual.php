dac3293da6a0db5e692eac2210e482088f14e478
Lewis Goddard <lewisgoddard@eustasy.org>
2018-02-24 11:20:34 +0000

    in SQL too

--- api/parts_manual.php
+++ api/parts_manual.php
@@ -6,7 +6,7 @@ $Ticket_ID = htmlentities($_GET['ticket'], ENT_QUOTES, 'UTF-8');
 $Description = htmlentities($_GET['description'], ENT_QUOTES, 'UTF-8');
 $Quantity = htmlentities($_GET['quantity'], ENT_QUOTES, 'UTF-8');
 
-$SQL = 'INSERT INTO `Parts` (`Ticket ID`, `STRC_CODE`, `STRC_CODE2`, `STRC_DESC`, `Quantity`) VALUES (\''.$Ticket_ID.'\', \'MANUAL_ENTRY\', \'MANUAL_ENTRY\', \''.$Description.'\', \''.$Quantity.'\');';
+$SQL = 'INSERT INTO `Parts` (`Ticket ID`, `STRC_CODE`, `STRC_CODE2`, `STRC_DESC`, `Quantity`) VALUES (\''.$Ticket_ID.'\', \'MANUAL_ENTRY\', \'MANUAL_ENTRY_'.$Time.'\', \''.$Description.'\', \''.$Quantity.'\');';
 $Result = mysqli_query($Sitewide['Database']['Connection'], $SQL);
 //var_dump($SQL);
 //var_dump($Result);
